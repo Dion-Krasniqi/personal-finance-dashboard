500f2e3b63b8c0f5e780f701acb29c023539402b
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-08-20 17:07:20 +0200

    Updated the previous CustomUser model and added Admin model with admin functions

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,16 +1,51 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser #django default model
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager # django default modelS
+
 
 
 
 
 # Models
 
-class CustomUser(AbstractUser):
+class CustomeUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+
+        if not email:
+            raise ValueError("Users must have an email address")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
+
+# class CustomUser(AbstractBaseUser, PermissionsMixin):
     # These are extra fields we're adding
-    email = models.EmailField(unique=True) # Make email unique
+#    email = models.EmailField(unique=True) # Make email unique
     # Custom field we've added
-    monthly_budget = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+#    monthly_budget = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+#    def __str__(self):
+#        return self.username
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomeUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return self.username
+        return self.email
+
