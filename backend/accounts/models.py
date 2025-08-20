a423395ddded553be877b0c453a5e4eb0e83ae10
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-08-20 16:31:58 +0200

    Created simple CustomUser model built on the AbstractUser model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser #django default model
 
-# Create your models here.
+
+
+
+# Models
+
+class CustomUser(AbstractUser):
+    # These are extra fields we're adding
+    email = models.EmailField(unique=True) # Make email unique
+    # Custom field we've added
+    monthly_budget = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "accounts.CustomUser" # We're telling django to use this instead of the built-in
