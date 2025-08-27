bccf857adbf60c6aa4fcb0f17de35a4fde7784fc
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-08-27 18:33:17 +0200

    Created the list_transaction view and the corresponding html page

--- backend/finance/urls.py
+++ backend/finance/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("add/", views.add_transaction, name = "add_transaction"),
+    path('add/', views.add_transaction, name = 'add_transaction'),
+    path('list/', views.list_transactions, name = 'list_transactions'),
 ]
--- backend/finance/views.py
+++ backend/finance/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import TransactionForm
+from .models import Transaction
 
 # Create your views here.
 @login_required
@@ -11,9 +12,15 @@ def add_transaction(request):
             transaction = form.save(commit = False)
             transaction.user = request.user
             transaction.save()
-            return redirect("transaction_List")
+            return redirect("list_transactions")
     else:
         form = TransactionForm()
 
     return render(request, "finance/add_transaction.html", {"form" : form})
 
+
+@login_required
+def list_transactions(request):
+    transactions = Transaction.objects.filter(user = request.user).order_by('-date')
+
+    return render(request, 'finance/list_transactions.html', {'transactions' : transactions})
