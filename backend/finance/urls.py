ae72be3a3b1845a48a06ff482b43273db127ad84
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-08-30 16:16:45 +0200

    Prepared the simple frontend system to recieve Plaid token

--- backend/finance/urls.py
+++ backend/finance/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('add/', views.add_transaction, name = 'add_transaction'),
     path('list/', views.list_transactions, name = 'list_transactions'),
+    path('get-plaid-token/', views.get_plaid_token, name = 'get_plaid_token'),
 ]
